29852e14e15fd9f203a8ecbe72ff4c80250cbb75
RaymondHKang <68756586+RaymondHKang@users.noreply.github.com>
2024-05-14 14:43:59 -0700

    x

--- model.py
+++ model.py
@@ -22,6 +22,7 @@ class LayerNorm(nn.Module):
         super().__init__()
         self.weight = nn.Parameter(torch.ones(ndim))
         self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
+        print(self.bias)
 
     def forward(self, input):
         return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
