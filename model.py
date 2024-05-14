4c713077fb851287b9fc26d08387500bd3c16887
RaymondHKang <68756586+RaymondHKang@users.noreply.github.com>
2024-05-14 14:33:07 -0700

    x

--- model.py
+++ model.py
@@ -58,10 +58,15 @@ class CausalSelfAttention(nn.Module):
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
 
+        tril = torch.tril(torch.ones(B, T, self.n_head, C // self.n_head).transpose(1, 2))
+        wei = torch.zeroes(((B, T, self.n_head, C // self.n_head).transpose(1, 2)))
+        wei = wei.masked_fill(tril == 0, float('-inf'))
+
+
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         if self.flash:
             # efficient attention using Flash Attention CUDA kernels
-            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=wei, dropout_p=self.dropout if self.training else 0, is_causal=False)
         else:
             # manual implementation of attention
             att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
