861f9ab6eac9294a2ccc1ad9427b7d7665927ef0
RaymondHKang <68756586+RaymondHKang@users.noreply.github.com>
2024-05-14 15:48:57 -0700

    x

--- model.py
+++ model.py
@@ -50,7 +50,7 @@ class CausalSelfAttention(nn.Module):
                                         .view(1, 1, config.block_size, config.block_size))
 
     def forward(self, x):
-        window_size = 10
+        window_size = 3
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
