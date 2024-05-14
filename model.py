28dc9a7dfa8ac3176e31453fa87a1c422b87217f
RaymondHKang <68756586+RaymondHKang@users.noreply.github.com>
2024-05-14 15:56:20 -0700

    x

--- model.py
+++ model.py
@@ -65,22 +65,22 @@ class CausalSelfAttention(nn.Module):
 
 
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        if self.flash:
-            # efficient attention using Flash Attention CUDA kernels
-            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
-        else:
+        # if self.flash:
+        #     # efficient attention using Flash Attention CUDA kernels
+        #     y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+        # else:
             # manual implementation of attention
-            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-            tril = torch.tril(torch.ones(T,T))
-            mask = torch.tril(torch.ones_like(tril), diagonal=window_size * (-1))
-            # Apply the mask to zero out the shifted lower triangle
-            tril[mask == 1] = 0
-            #att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-            att = att.masked_fill(tril == 0, float('-inf'))
-            att = F.softmax(att, dim=-1)
-            print(att)
-            att = self.attn_dropout(att)
-            y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        tril = torch.tril(torch.ones(T,T))
+        mask = torch.tril(torch.ones_like(tril), diagonal=window_size * (-1))
+        # Apply the mask to zero out the shifted lower triangle
+        tril[mask == 1] = 0
+        #att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = att.masked_fill(tril == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        print(att)
+        att = self.attn_dropout(att)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
 
         # output projection
